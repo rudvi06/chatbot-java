d4069ee0a7900eb8e47978b011a1e2f84edd1fd8
rudvi06 <rudvi306@gmail.com>
2023-01-11 18:08:22 +0530

    chatbot first commit

---
+++ src/main/java/com/project/chatbot/ChatbotApplication.java
@@ -0,0 +1,14 @@
+package com.project.chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class ChatbotApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotApplication.class, args);
+	}
+
+}
